b294f5e9d3153fa4b5c5dd871f04915980045c43
Priyansh Jain <48293372+ErR0rpj@users.noreply.github.com>
2020-02-06 20:00:17 +0530

    3

--- app/src/main/java/com/example/contacts/Main2Activity.java
+++ app/src/main/java/com/example/contacts/Main2Activity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 
 public class Main2Activity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/contacts/MainActivity.java
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -18,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BTNcontact= findViewById(R.id.BTNcontact);
+        TVcontact= findViewById(R.id.TVcontact);
+
         BTNcontact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
