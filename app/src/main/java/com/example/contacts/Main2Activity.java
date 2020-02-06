fec9be1a452cb9a1afc9750268a13a2867c03f63
Priyansh Jain <48293372+ErR0rpj@users.noreply.github.com>
2020-02-06 20:09:41 +0530

    Second Activity coding begins

--- app/src/main/java/com/example/contacts/Main2Activity.java
+++ app/src/main/java/com/example/contacts/Main2Activity.java
@@ -3,12 +3,26 @@ package com.example.contacts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class Main2Activity extends AppCompatActivity {
 
+    EditText ETname,ETnumber,ETemail,ETaddress;
+    ImageView IVpoty,IVkutta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ETname= findViewById(R.id.ETname);
+        ETnumber= findViewById(R.id.ETnumber);
+        ETemail= findViewById(R.id.ETemail);
+        ETaddress= findViewById(R.id.ETaddress);
+        IVpoty= findViewById(R.id.IVpoty);
+        IVkutta= findViewById(R.id.IVkutta);
+
+        
+
     }
 }
