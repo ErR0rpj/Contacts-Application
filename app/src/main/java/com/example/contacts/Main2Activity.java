abc2a9c13c0f46b29a05a8002e9e14f0ef2d3f87
Priyansh Jain <48293372+ErR0rpj@users.noreply.github.com>
2020-02-07 00:58:20 +0530

    final

--- app/src/main/java/com/example/contacts/Main2Activity.java
+++ app/src/main/java/com/example/contacts/Main2Activity.java
@@ -2,6 +2,7 @@ package com.example.contacts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -35,6 +36,45 @@ public class Main2Activity extends AppCompatActivity {
                     Toast.makeText(Main2Activity.this,"Sare details bharna zaruri h",Toast.LENGTH_SHORT).show();
                 }
                 else{
+                    String name= ETname.getText().toString();
+                    String number= ETnumber.getText().toString().trim();
+                    String email= ETemail.getText().toString().trim();
+                    String address= ETaddress.getText().toString();
+                    int pic=1;
+                    Intent intent= new Intent();
+                    intent.putExtra("name",name);
+                    intent.putExtra("number",number);
+                    intent.putExtra("email",email);
+                    intent.putExtra("address",address);
+                    intent.putExtra("pic",pic);
+                    setResult(RESULT_OK,intent);
+                    Main2Activity.this.finish();
+                }
+            }
+        });
+
+        IVkutta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean fields = (ETname.getText().toString().isEmpty()) | (ETnumber.getText().toString().isEmpty()) | (ETemail.getText().toString().isEmpty()) | (ETaddress.getText().toString().isEmpty());
+                if (ETaddress.getText().toString().isEmpty()) {
+                    Toast.makeText(Main2Activity.this, "Shi me post nhi bhejunga, Postal Address daal", Toast.LENGTH_LONG).show();
+                }
+                else if (fields) {
+                    Toast.makeText(Main2Activity.this, "Sare details bharna zaruri h", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    String name = ETname.getText().toString();
+                    String number = ETnumber.getText().toString().trim();
+                    String email = ETemail.getText().toString().trim();
+                    String address = ETaddress.getText().toString();
+                    Intent intent = new Intent();
+                    intent.putExtra("name", name);
+                    intent.putExtra("number", number);
+                    intent.putExtra("email", email);
+                    intent.putExtra("address", address);
+                    intent.putExtra("pic",2);
+                    setResult(RESULT_OK, intent);
                     Main2Activity.this.finish();
                 }
             }
--- app/src/main/java/com/example/contacts/MainActivity.java
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -1,33 +1,88 @@
 package com.example.contacts;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView TVcontact;
     Button BTNcontact;
+    ImageView IVcall, IVsearch, IVlocation;
+    final int resultfrom2 = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BTNcontact= findViewById(R.id.BTNcontact);
-        TVcontact= findViewById(R.id.TVcontact);
+        BTNcontact = findViewById(R.id.BTNcontact);
+        TVcontact = findViewById(R.id.TVcontact);
+        IVcall = findViewById(R.id.IVcall);
+        IVsearch = findViewById(R.id.IVsearch);
+        IVlocation = findViewById(R.id.IVlocation);
+
+        IVcall.setVisibility(View.GONE);
+        IVlocation.setVisibility(View.GONE);
+        IVsearch.setVisibility(View.GONE);
 
         BTNcontact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(MainActivity.this, com.example.contacts.Main2Activity.class);
-                startActivity(intent);
+                Intent intent = new Intent(MainActivity.this, com.example.contacts.Main2Activity.class);
+                startActivityForResult(intent, resultfrom2);
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == resultfrom2) {
+            if (resultCode == RESULT_OK) {
+                IVlocation.setVisibility(View.VISIBLE);
+                IVcall.setVisibility(View.VISIBLE);
+                if (data.getIntExtra("pic", 0) == 1) {
+                    IVsearch.setImageResource(R.drawable.poty);
+                    IVsearch.setVisibility(View.VISIBLE);
+                }
+                else if (data.getIntExtra("pic", 0) == 2) {
+                    IVsearch.setImageResource(R.drawable.kutta);
+                    IVsearch.setVisibility(View.VISIBLE);
+                }
+                final String number= data.getStringExtra("number");
+                final String address= data.getStringExtra("address");
+
+                IVcall.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+number));
+                        startActivity(intent);
+                    }
+                });
+
+                IVlocation.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent= new Intent(Intent.ACTION_VIEW,Uri.parse("geo:0,0?q="+address));
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(MainActivity.this, "Bharo or kisi pe pic ko click kro", Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 }
